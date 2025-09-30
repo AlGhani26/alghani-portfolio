7fb1e8025af1cd4c5cf7594bcd8eca8fab2a7036
alghani26 <alghani63@gmail.com>
2025-09-30 12:57:49 +0700

    fix bug

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ import { FloatingNav } from '@/components/ui/FloatingNavbar';
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col 
-    overflow-hidden mx-auto sm:px-10 px-5">
+    mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
           <FloatingNav navItems={navItems}/>
           <Hero />
